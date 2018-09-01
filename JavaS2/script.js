f46f5152423993b6dec11fe259beab0499303008
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-01 15:21:24 -0400

    Add files via upload

---
+++ JavaS2/script.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 20;
+
+if (a < b) {
+    var result = a + b;
+    document.write("The result of a and b is: ", result);
+} else {
+    document.write("the data is not valid");
+
+}
