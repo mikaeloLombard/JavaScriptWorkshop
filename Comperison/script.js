d85ae13ac68f207266d8f8b2ed2f660540f12fea
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-01 22:50:26 -0400

    Add files via upload

---
+++ Comperison/script.js
@@ -0,0 +1,13 @@
+var a = 20;
+var b = 20;
+var c = 50;
+var d = 40;
+
+if (a >= b || c <=d) {
+    document.write("Condition is true");
+}else {
+    document.write("Condition is not true");
+}
+
+// && means and 
+// || means or
