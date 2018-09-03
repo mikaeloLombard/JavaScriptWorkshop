9b82e860893d2931ab13b63fe9b57a44370bb074
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-02 22:27:55 -0400

    Add files via upload

---
+++ Increment and Decrement/script.js
@@ -0,0 +1,14 @@
+var a = 8;
+var b = 6;
+var c = (a > b) ? alert("true"): alert("false");
+
+//var c = "a is greater than b";
+
+/*
+if ( a > b) {
+    alert(c);
+}else {
+    alert("a is not greater than b")
+} 
+
+*/
