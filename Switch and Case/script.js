d2e7344c355ccee4355df8be048886cc4ffabf73
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-04 23:55:26 -0400

    Add files via upload

---
+++ Switch and Case/script.js
@@ -0,0 +1,20 @@
+var fruit = "orange";
+
+switch(fruit) {
+    case "apple":
+        document.write("apple is 2$");
+        break;
+
+    case "banana":
+        document.write("banana is 4$");
+        break;
+
+    case "orange":
+        document.write("orange is 3$");
+        break;
+
+    default:
+        document.write("Fruit is not available");
+
+
+}
