9d549f3471546a9b72e4569dc60949823280c7cf
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-09 22:18:57 -0400

    Add files via upload

---
+++ Loops/script.js
@@ -0,0 +1,13 @@
+var count = 0;
+
+while (count < 10) {
+    document.write("Hello world! <br />");
+    count++;
+
+} 
+
+do{
+    document.write("This is do while loop <br /n>");
+    count++;
+
+}while (count < 15)
