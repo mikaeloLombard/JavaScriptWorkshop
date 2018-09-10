7fd0357455e51575c854cd90e3682f830a47a96f
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-09 22:18:11 -0400

    Add files via upload

---
+++ Array/script.js
@@ -0,0 +1,23 @@
+var studentName = [];
+studentName [0] = "Mika";
+studentName [1] = "George";
+studentName [2] = "Dilly";
+studentName [3] = "Gabby";
+studentName [4] = "Paco";
+
+function addName(){
+    //Code goes here
+}
+var teacherName = ["Computer", 100, "Cost", true];
+document.write (studentName [2]);
+document.write (teacherName.length);
+
+alert(teacherName[3]);
+
+var newArray = Array(5);
+teacherName.length();
+// lenght - takes the value number of slots in the list
+
+teacherName.addName();
+
+
