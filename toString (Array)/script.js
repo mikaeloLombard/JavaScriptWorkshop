13f45ccd7108b68fbcef045b513b9662b7780551
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-09 22:17:23 -0400

    Add files via upload

---
+++ toString (Array)/script.js
@@ -0,0 +1,11 @@
+var studentName = [];
+studentName [0] = 10;
+studentName [1] = 20;
+studentName [2] = 30;
+studentName [3] = 40;
+studentName [4] = 50;
+
+stundentName.toString();
+// this will get all the tags starting with a 
+// and will store them in an array accordingly
+var linksA = document.getElementsByTagName("a");
