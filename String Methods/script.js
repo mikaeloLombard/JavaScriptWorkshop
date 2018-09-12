e5ded0350912f00c1d4da9fd58a7a7f8590c6c72
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-11 22:22:37 -0400

    Add files via upload

---
+++ String Methods/script.js
@@ -0,0 +1,18 @@
+var a = "thi's is\" a string";
+var b = a.charAt(2);
+var c = a.length;
+var d = a.search("a");
+var date = new Date()
+var getYear = date.getFullYear();
+var hours = date.getHours();
+document.write(b + "<br>");
+document.write(c + "<br>");
+document.write(d + "<br>");
+document.write(date + "<br>");
+document.write(getYear + "<br>");
+document.write(hours);
+
+
+
+
+
