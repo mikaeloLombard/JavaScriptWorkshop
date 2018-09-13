bfe97dda562d6b64a01716db626536a8f1a543da
Miguel Lopez <miguelcodeworld@gmail.com>
2018-09-12 22:56:21 -0400

    Add files via upload

---
+++ Object in JavaScript/script.js
@@ -0,0 +1,15 @@
+// This script will not run
+// it is intended to learn how to create objects.
+
+var name = "Mikaelo";
+var age = 25;
+var proffesion = "programmer";
+
+var person = new Object();
+person.pName = "Mikaelo";
+person.pAge = 100;
+person.pProfession = "Student";
+
+
+var student = {name: "Tom", grade: 70, class: 5};
+
